91097a3f8557a2d98d27cc219029c53f5479ce45
PRIYASAKI <125540653+PRIYASAKI@users.noreply.github.com>
2025-05-23 01:06:56 +0530

    ninth commit

--- server/server.js
+++ server/server.js
@@ -29,7 +29,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://prs-fabrics-front.onrender.com"
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
